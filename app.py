c4bdfb924be038feba91ac4f8bb498f96d3f5b67
Thinh <lthinh25@gmail.com>
2023-01-31 21:30:03 -0800

    social icons and links

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def index():  # put application's code here
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return "WIP"
+
+
 if __name__ == '__main__':
     app.run()
